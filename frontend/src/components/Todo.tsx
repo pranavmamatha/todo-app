7c88b97279fd8261e3319499dc1196a2b1973e5d
pranavranjithkannan <pranavranjithkannan@gmail.com>
2024-12-13 20:23:03 +0530

    fixed some logical errors

--- frontend/src/components/Todo.tsx
+++ frontend/src/components/Todo.tsx
@@ -7,25 +7,29 @@ function Todo(props: {
   fetchTodo: () => void;
 }) {
   async function handleClick(a: string) {
-    const myHeader = new Headers();
-    myHeader.append("id", "0");
-    myHeader.append("Content-type", "application/json");
-    const myBody = JSON.stringify({
-      title: props.title,
-      description: props.description,
-      completed: a == "checkbox" ? !props.completed : props.completed,
-    });
-    const fetchData = await fetch(
-      `${import.meta.env.VITE_BACKEND_URL}todos/update-todo/${props._id}`,
-      {
-        method: "PUT",
-        headers: myHeader,
-        body: myBody,
-      }
-    );
-    const data = await fetchData.json();
-    console.log(data.message);
-    props.fetchTodo();
+    try {
+      const myHeader = new Headers();
+      myHeader.append("id", "0");
+      myHeader.append("Content-type", "application/json");
+      const myBody = JSON.stringify({
+        title: props.title,
+        description: props.description,
+        completed: a == "checkbox" ? !props.completed : props.completed,
+      });
+      const fetchData = await fetch(
+        `${import.meta.env.VITE_BACKEND_URL}todos/update-todo/${props._id}`,
+        {
+          method: "PUT",
+          headers: myHeader,
+          body: myBody,
+        }
+      );
+      const data = await fetchData.json();
+      console.log(data.message);
+      props.fetchTodo();
+    } catch (error) {
+      console.log("Error", error);
+    }
   }
   return (
     <div>
